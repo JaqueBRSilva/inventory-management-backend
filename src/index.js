03f66bf4695dad9856ab1b7ec346273c6ab80aa9
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-07-23 16:43:49 -0300

    refactor: movido as rotas de `/produtos` p/ arq. dedicado

--- src/index.js
+++ src/index.js
@@ -1,56 +1,14 @@
 import express from "express";
+import productsRouter from "./routers/produtosRouter.js";
 
-const server = express();
-server.use(express.json());
+const server = express()
+server.use(express.json()) // TODAS AS REQUISIÇÕES CONVERTIDAS PARA JSON
 
-// const PORT = 3000 || process.env.DEFAULT_PORT
 const PORT = 3000;
 
-const produtos = [
-  {
-    name: 'Smart TV Samsung 32"',
-    quantidade: 2000,
-    preco: 1500,
-  },
-  {
-    name: "Notebook Acer Nitro 5",
-    quantidade: 1000,
-    preco: 5500,
-  },
-  {
-    name: "Gabinete Gamer 16GB i59400",
-    quantidade: 1000,
-    preco: 4500,
-  },
-];
-
-server.get("/produtos", (req, res) => {
-  res.status(200).json(produtos);
-});
-
-server.get("/produtos/:id_cod", (req, res) => {
-  const id = Number(req.params.id_cod);
-
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ msg: "Digite apenas números" });
-  }
-
-  const dados = produtos[id - 1];
-
-  if (dados) {
-    return res.status(200).json(dados);
-  } else {
-    return res.status(404).json({ msg: "Produto Não Encontrado" });
-  }
-});
-
-server.post("/produtos", (req, res) => {
-  const produto = req.body;
-  produtos.push(produto);
-  res.status(201).json({ msg: "Produto Cadastrado com Sucesso" });
-});
+server.use(productsRouter)
 
 server.listen(PORT, () => {
   console.info(`>>> SERVIDOR EM EXECUÇÃO NA PORTA: ${PORT} <<<`);
-  console.warn(`ACESSE - http://localhost:${PORT}`);
+  console.warn(`http://localhost:${PORT}`);
 });
