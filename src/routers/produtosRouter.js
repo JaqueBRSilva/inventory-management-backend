d489f971625e51eaa1bca24496a0a2ab40ac58a0
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-07-24 14:50:31 -0300

    refactor: separado parte 'services' do 'router'

--- src/routers/produtosRouter.js
+++ src/routers/produtosRouter.js
@@ -1,20 +1,14 @@
 import express from "express";
-import { getFirestore } from "firebase-admin/firestore";
-import app from "../firebase/app.js";
+import { findAll, findById } from "../services/produtosService.js";
 
 const produtosRouter = express.Router();
-const db = getFirestore(app);
 
 // GET
 produtosRouter.get("/produtos", async (req, res) => {
     try {
-        const documents = await db.collection("produtos").get();
-        const produtos = [];
-        documents.forEach(doc => {
-            const produto = { id: doc.id, ...doc.data() }
-            produtos.push(produto)
-        });
+        const produtos = await findAll()
         return res.status(200).json(produtos);
+
     } catch (error) {
         return res.status(500).json({ msg: "Erro interno no servidor." });
     }
@@ -24,11 +18,9 @@ produtosRouter.get("/produtos", async (req, res) => {
 produtosRouter.get("/produtos/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        const doc = await db.collection("produtos").doc(id).get();
+        const produto = await findById(id)
 
-        if (doc.exists) {
-            // const produto = doc.data()
-            const produto = { id: doc.id, ...doc.data() };
+        if (produto) {
             return res.status(200).json(produto);
         } else {
             return res.status(404).json({ msg: "Produto não encontrado." });
@@ -42,7 +34,7 @@ produtosRouter.get("/produtos/:id", async (req, res) => {
 produtosRouter.post("/produtos", async (req, res) => {
     try {
         const produto = req.body;
-        await db.collection("produtos").add(produto);
+        await save(produto)
         return res.status(201).json({ msg: "Produto cadastrado." });
     } catch (error) {
         return res.status(500).json({ msg: "Erro interno no servidor." });
@@ -52,14 +44,11 @@ produtosRouter.post("/produtos", async (req, res) => {
 // PUT
 produtosRouter.put("/produtos/:id", async (req, res) => {
     try {
-        const id = req.params.id;
-        const produto = req.body;
+        const id = req.params.id
+        const produto = req.body
+        const flag = await update(id, produto)
 
-        const docRef = db.collection("produtos").doc(id);
-        const doc = await docRef.get();
-
-        if (doc.exists) {
-            await docRef.update(produto);
+        if (flag) {
             return res.status(200).json({ msg: "Produto alterado." });
         } else {
             return res.status(404).json({ msg: "Produto não encontrado." });
@@ -73,11 +62,9 @@ produtosRouter.put("/produtos/:id", async (req, res) => {
 produtosRouter.delete("/produtos/:id", async (req, res) => {
     try {
         const id = req.params.id;
-        const docRef = db.collection("produtos").doc(id);
-        const doc = await docRef.get();
+        const flag = await remove(id)
 
-        if (doc.exists) {
-            await docRef.delete();
+        if (flag) {
             return res.status(200).json({ msg: "Produto excluido." });
         } else {
             return res.status(404).json({ msg: "Produto não encontrado." });
