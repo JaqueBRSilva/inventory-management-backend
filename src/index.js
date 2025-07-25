8c8bad29b68f0560927752dae855a3ffef4b4f14
JaqueBRSilva <j.silvabroccolo@gmail.com>
2025-07-25 14:51:49 -0300

    feat: add configurações de `dotenv` no index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import produtosRouter from "./routers/produtosRouter.js";
 import usuariosRouter from "./routers/usuariosRouter.js";
@@ -5,7 +6,7 @@ import usuariosRouter from "./routers/usuariosRouter.js";
 const server = express()
 server.use(express.json()) // TODAS AS REQUISIÇÕES CONVERTIDAS PARA JSON
 
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT
 
 server.use(produtosRouter)
 server.use(usuariosRouter)
